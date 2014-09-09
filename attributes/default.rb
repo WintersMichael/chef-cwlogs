9320f63bd41a659b6d3055f2fc419bf4a388fef1
mikeschuette <mike.schuette@gmail.com>
2014-09-09 03:00:18 -0500

    Initial commit

---
+++ attributes/default.rb
@@ -0,0 +1,2 @@
+default[:cwlogs][:logfile] = '/var/log/aws/opsworks/opsworks-agent.log'
+default[:cwlogs][:region] = "us-east-1"
---
+++ metadata.rb
@@ -0,0 +1,10 @@
+maintainer       "Mike Schuette"
+maintainer_email "mike.schuette@gmail.com"
+license          "FreeBSD"
+description      "Installs and configures AWS CloudWatch Logs"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
+
+%w{ ubuntu debian centos redhat fedora }.each do |os|
+	supports os
+end
