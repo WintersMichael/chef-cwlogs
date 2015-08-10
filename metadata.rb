b348714bb52e1a7cc7bb5e2a6b1cc909cba5cb65
Ben Bridts <ben.bridts@gmail.com>
2015-08-10 09:37:36 +0200

    Update metadata

--- metadata.rb
+++ metadata.rb
@@ -1,11 +1,12 @@
-name             'cwlogs'
-maintainer       'Mike Schuette'
-maintainer_email 'mike.schuette@gmail.com'
+name             'cloudwatch-logs'
+maintainer       'Ben Bridts'
+maintainer_email 'ben@cloudar.be'
 license          'FreeBSD'
+source_url       'https://github.com/WeAreCloudar/chef-cloudwatch-logs'
 description      'Installs and configures AWS CloudWatch Logs'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.0.3'
 
-%w{ ubuntu debian centos redhat fedora }.each do |os|
+%w{ ubuntu debian centos redhat fedora amazon}.each do |os|
   supports os
 end
