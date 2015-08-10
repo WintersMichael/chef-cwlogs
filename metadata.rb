dc509ddcab8357a9f91cbb4dc496e86a2f9c6937
Ben Bridts <ben.bridts@gmail.com>
2015-08-10 12:13:39 +0200

    Remove chef 12 specific attribute

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,6 @@ name             'cloudwatch-logs'
 maintainer       'Ben Bridts'
 maintainer_email 'ben@cloudar.be'
 license          'FreeBSD'
-source_url       'https://github.com/WeAreCloudar/chef-cloudwatch-logs'
 description      'Installs and configures AWS CloudWatch Logs'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.0.3'
