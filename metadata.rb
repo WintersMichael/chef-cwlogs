56db2d4fab8f5a026892c3b96b835913226982d5
Ben Bridts <ben.bridts@gmail.com>
2015-08-10 12:16:47 +0200

    Increase version number

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'ben@cloudar.be'
 license          'FreeBSD'
 description      'Installs and configures AWS CloudWatch Logs'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.0.3'
+version          '0.0.4'
 
 %w{ ubuntu debian centos redhat fedora amazon}.each do |os|
   supports os
