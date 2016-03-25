da7177ca66b3323e8b3b1e11dc433c00312bc6f0
Ivan Suftin <isuftin@usgs.gov>
2016-03-25 08:49:13 -0500

    Updated the process name that the installer checks for to decide whether awslogs is already installed or not

--- recipes/installer.rb
+++ recipes/installer.rb
@@ -26,5 +26,5 @@ end
 
 execute 'Install CloudWatch Logs agent' do
   command "/opt/aws/cloudwatch/awslogs-agent-setup.py -n -r #{node['cwlogs']['region']} -c /tmp/cwlogs.cfg"
-  not_if { system 'pgrep -f aws-logs-agent-setup' }
+  not_if { system 'pgrep -f awslogs' }
 end
